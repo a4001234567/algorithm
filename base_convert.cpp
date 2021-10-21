6ef71c963c9e3d50ba0296e99a01543fcbe4746b
a4001234567 <48621584+a4001234567@users.noreply.github.com>
2021-10-21 12:44:01 +0800

    Create base_convert.cpp

---
+++ base_convert.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <stack>
+using namespace std;
+const int p = -2;
+void convert(int ori){
+    stack<int> digits;
+    while (ori) {
+        if (ori%p >= 0){
+            digits.push(ori%p);
+            ori/=p;
+        }
+        else {
+            digits.push(-p+(ori%p));
+            ori/=p;ori++;
+        }
+    }
+    while (!digits.empty()){
+        printf("%d ",digits.top());
+        digits.pop();
+    }
+}
+int main(){
+    convert(-7);
+    puts("\n");
+    convert(14);
+    return 0;
+}
